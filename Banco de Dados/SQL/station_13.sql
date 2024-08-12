6dd5fc5144d57a5f6a05326e4ccbdadf4f40d71a
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-08-12 15:41:39 -0300

    Add SQL Exercises

--- Banco de Dados/SQL/station_13.sql
+++ Banco de Dados/SQL/station_13.sql
@@ -1,35 +1,26 @@
-WITH cities_length AS (
-    SELECT 
-        name, 
-        CHAR_LENGTH(name) AS length
-    FROM CITY
-    ORDER BY name
+WITH nomes_tams AS (
+    SELECT CITY, CHAR_LENGTH(CITY) AS TAM
+    FROM STATION
 ),
-min_max_length AS (
-    SELECT min(length) AS MIN, max(length) AS MAX
-    FROM cities_length
+max_min AS (
+    SELECT MAX(TAM) AS MAX_TAM, MIN(TAM) AS MIN_TAM
+    FROM nomes_tams
 ),
-nomes_menores AS (
-    SELECT *, ROW_NUMBER() OVER(PARTITION BY length ORDER BY name) AS line
-    FROM cities_length
-    WHERE length IN (SELECT MIN FROM min_max_length)
+maior AS (
+    SELECT CITY AS NOME_MAIOR, TAM AS TAM_MAIOR
+    FROM nomes_tams
+    WHERE TAM IN (SELECT MAX_TAM FROM max_min)
+    ORDER BY CITY
+    LIMIT 1
 ),
-nomes_maiores AS (
-    SELECT *, ROW_NUMBER() OVER(PARTITION BY length ORDER BY name) AS line
-    FROM cities_length
-    WHERE length IN (SELECT MAX FROM min_max_length)
-),
-menor_target AS (
-    SELECT name AS NOME_MENOR, length AS TAM_MENOR
-    FROM nomes_menores
-    WHERE line = 1
-),
-maior_target AS (
-    SELECT name AS NOME_MAIOR, length AS TAM_MAIOR
-    FROM nomes_maiores
-    WHERE line = 1
+menor AS (
+    SELECT CITY AS NOME_MENOR, TAM AS TAM_MENOR
+    FROM nomes_tams
+    WHERE TAM IN (SELECT MIN_TAM FROM max_min)
+    ORDER BY CITY
+    LIMIT 1
 )
 
 SELECT NOME_MENOR, NOME_MAIOR, TAM_MENOR, TAM_MAIOR
-FROM menor_target
-JOIN maior_target
+FROM maior
+JOIN menor
