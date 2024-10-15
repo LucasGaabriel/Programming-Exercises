b959dc143dcc07ccb0e491d1c5bbc596773fa65d
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-10-15 11:51:23 -0300

    Update fundamentos Next.js

--- Desenvolvimento Web - Cod3r/Next/fundamentos/src/pages/estiloso.jsx
+++ Desenvolvimento Web - Cod3r/Next/fundamentos/src/pages/estiloso.jsx
@@ -1,11 +1,13 @@
+import Layout from '@/components/Layout'
 import styles from '../styles/Estiloso.module.css'
 import Link from 'next/link'
 
 export default function Estiloso() {
     return (
-        <div className={styles.roxo}>
-            <Link href="/">Voltar</Link>
-            <h1>Estilo usando CSS Módulos</h1>
-        </div>
+        <Layout titulo="Exemplo de CSS Modularizado">
+            <div className={styles.roxo}>
+                <h1>Estilo usando CSS Módulos</h1>
+            </div>
+        </Layout>
     )
 }
